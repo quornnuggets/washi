991322c82dccdf0d22707b2b70a2914f72a4d5e8
emma! <emma.whyte32@yahoo.co.uk>
2026-07-30 22:22:19 +0100

    force refresh for app icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      
+      includeAssets: [
+        "washi-icon-180.png",
+        "pwa-192x192.png",
+        "pwa-512x512.png",
+      ],
 
-manifest: {
-  name: "Washi",
-  short_name: "Washi",
+      manifest: {
+        name: "Washi",
+        short_name: "Washi",
+        description: "Adam's laundry buddy",
+        theme_color: "#f4eee4",
+        background_color: "#f4eee4",
+        display: "standalone",
 
-  display: "standalone",
-
-  theme_color: "#f4eee4",
-  background_color: "#f4eee4",
-
-  icons: [
-    {
-      src: "/pwa-192x192.png",
-      sizes: "192x192",
-      type: "image/png",
-    },
-    {
-      src: "/pwa-512x512.png",
-      sizes: "512x512",
-      type: "image/png",
-    },
-    {
-      src: "/pwa-512x512.png",
-      sizes: "512x512",
-      type: "image/png",
-      purpose: "maskable"
-    }
-  ]
-}
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
