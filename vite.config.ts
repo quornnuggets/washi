ab4379ca6e3556b83dba366a20e4d9059b128a1d
emma! <emma.whyte32@yahoo.co.uk>
2026-07-30 22:06:52 +0100

    app icon

--- vite.config.ts
+++ vite.config.ts
@@ -5,3 +5,32 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+manifest: {
+  name: "Washi",
+  short_name: "Washi",
+
+  display: "standalone",
+
+  theme_color: "#f4eee4",
+  background_color: "#f4eee4",
+
+  icons: [
+    {
+      src: "/pwa-192x192.png",
+      sizes: "192x192",
+      type: "image/png",
+    },
+    {
+      src: "/pwa-512x512.png",
+      sizes: "512x512",
+      type: "image/png",
+    },
+    {
+      src: "/pwa-512x512.png",
+      sizes: "512x512",
+      type: "image/png",
+      purpose: "maskable"
+    }
+  ]
+}
